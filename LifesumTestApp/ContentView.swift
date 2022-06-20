d76cce648591b0e3419209a03b789a635661b2bd
Guglielmo Chimera <skies.harpies_00@icloud.com>
2022-06-20 17:21:09 +0200

    SwiftUI app setup

---
+++ LifesumTestApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LifesumTestApp
+//
+//  Created by Guglielmo Chimera on 20/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LifesumTestApp/LifesumTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifesumTestAppApp.swift
+//  LifesumTestApp
+//
+//  Created by Guglielmo Chimera on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LifesumTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
